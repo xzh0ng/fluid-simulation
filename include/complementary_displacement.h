bcaf2a3c435b4de1c98e3189ba713f9710ded506
Xuhong Zhong <xuhong.zhong@mail.utoronto.ca>
2020-12-05 19:16:42 -0500

    add one overload

--- include/complementary_displacement.h
+++ include/complementary_displacement.h
@@ -1,6 +1,60 @@
-#ifndef COMPLEMENTARY_DISPLACEMENT_H
-#define COMPLEMENTARY_DISPLACEMENT_H
 #include <Eigen/Core>
+#include <Eigen/Sparse>
+#include<Eigen/SparseCholesky>
+using namespace Eigen;
+// Assume the input animation and simulation use the same tetrahedron mesh
+// 
+// Given the rest mesh (V,T), at the frame t, we find the complementary displacement Uc
+// to add dynamics secondary effects to the input rig displacement Ur.
+//
+// Inputs:
+//   V         #V by 3 list of rest mesh vertex positions
+//   T         #T by 4 list tetrahedra indices into rows of V
+//   M         #V*3 by #V*3 sparse mass matrix
+//   Ur        #V by 3 rig displacement at the current frame
+//   U_prev    #V by 3 final displacements at the last frame
+//   Udot_prev #V by 3 final speed at the last frame
+//   J         #V*3 by m dense rig jacobian at the current frame
+//   g         #V*3 by 1 gradient of the elasticity potential
+//   H         #V*3 by #V*3 hessian of the elasticity potential
+//   dt        time step
+//   f         #V*3 by 1 external force
+//   energy(V,T,Ur,Uc,dt) a function that computes the total energy of the FEM system (for doing line search)
+// Outputs:
+//   Uc #V by 3 complementary displacement at the current frame
+template<typename ENERGY>
+void complementary_displacement(
+  const Eigen::MatrixXd & V, 
+  const Eigen::MatrixXi & T, 
+  const Eigen::SparseMatrix<double> & M,
+  const Eigen::MatrixXd & Ur,
+  const Eigen::MatrixXd & U_prev,
+  const Eigen::MatrixXd & Udot_prev,
+  const Eigen::MatrixXd & J,
+  const Eigen::MatrixXd & g,
+  const Eigen::SparseMatrix<double> & H,
+  const double & dt,
+  const Eigen::MatrixXd & f,
+  ENERGY & energy,
+  Eigen::MatrixXd & Uc) {
+    SparseMatrix<double> Q = H + dt * dt * M;
+    MatrixXd temp = ((Ur - U_prev) / dt - Udot_prev);
+    VectorXd temp_v = Map<VectorXd>(temp.data(), temp.size());
+    MatrixXd l = -g + ((M / dt) * temp_v) + f;
+    MatrixXd C = J.transpose() * M;
+
+    int m = C.rows(), dn = Q.rows();
+    MatrixXd A(dn + m, dn + m);
+    A << MatrixXd(Q), C.transpose(), C, MatrixXd::Zero(m, m);
+    VectorXd b(dn + m);
+    b << l, VectorXd::Zero(m);
+    SimplicialLDLT<SparseMatrix<double>> solver;
+    Uc = solver.compute(A.sparseView()).solve(b);
+
+    // line search
+    
+  }
+
 // Assume the input animation and simulation use the same tetrahedron mesh
 // 
 // Given the rest mesh (V,T), at the frame t, we find the complementary displacement Uc
@@ -29,5 +83,10 @@ void complementary_displacement(
   const Eigen::SparseMatrix<double> & H,
   const double & dt,
   ENERGY & energy,
-  Eigen::MatrixXd & Uc);
-#endif
+  Eigen::MatrixXd & Uc) {
+      MatrixXd U_prev = MatrixXd::Zero(Ur.rows(), Ur.cols());
+      MatrixXd Udot_prev = MatrixXd::Zero(Ur.rows(), Ur.cols());
+      // no external forces for now
+      MatrixXd f = MatrixXd::Zero(Ur.rows(), Ur.cols());
+      complementary_displacement(V, T, M, Ur, U_prev, Udot_prev, J, g, H, dt, f, energy, Uc);
+  }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "include/complementary_displacement.h"
 #include <igl/read_triangle_mesh.h>
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/parula.h>
+#include <igl/massmatrix.h>
+#include <igl/boundary_facets.h>
 #include <Eigen/Core>
-
+using namespace Eigen;
 
 int main(int argc, char *argv[])
 {
@@ -12,9 +15,21 @@ int main(int argc, char *argv[])
   igl::read_triangle_mesh(
     (argc>1?argv[1]:"../data/cactus.obj"),V,F);
 
+  SparseMatrix<double> M;
+  igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+  
+  MatrixXi T;
+  igl::boundary_facets(T, F);
+  F = F.rowwise().reverse().eval();
+
+  // computes the total energy of the FEM system (for doing line search)
+  auto energy = [](Ref<const MatrixXd> V, Ref<const MatrixXi> T,
+      Ref<const MatrixXd> Ur, Ref<const MatrixXd> Uc, const double & dt) {
+      
+  };
+
   igl::opengl::glfw::Viewer viewer;
 
-  update();
   viewer.launch();
   return EXIT_SUCCESS;
 }
