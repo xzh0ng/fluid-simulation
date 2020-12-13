d133ca66d3a12850f41a867b66e0415e1a41a40d
Sitao Wang <sitaowang1998@outlook.com>
2020-12-13 13:36:14 -0500

    Improved advection

--- src/advection.cpp
+++ src/advection.cpp
@@ -4,7 +4,5 @@
 
 
 void advection(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt) {
-    for (size_t i = 0; i < q.size() / 3; ++i) {
-        q.segment<3>(3 * i) += qdot.segment<3>(3 * i) * dt;
-    }
+    q += qdot * dt;
 }
