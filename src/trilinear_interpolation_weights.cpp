12a2d6e45b5e70fe0f782dd6b7ddc1c2cb656293
Xuhong Zhong <xuhong.zhong@mail.utoronto.ca>
2020-12-14 12:46:42 -0500

    include header file and weights order consistent with description in header

--- src/trilinear_interpolation_weights.cpp
+++ src/trilinear_interpolation_weights.cpp
@@ -1,3 +1,6 @@
+#include <trilinear_interpolation_weights.h>
+using namespace Eigen;
+
 void trilinear_interpolation_weights(
   const int nx,
   const int ny,
@@ -12,18 +15,18 @@ void trilinear_interpolation_weights(
 
     // x, y, z
     W(0) = (1 - ds(0)) * (1 - ds(1)) * (1 - ds(2));
-    // x + 1, y, z
-    W(1) = ds(0) * (1 - ds(1)) * (1 - ds(2));
+    // x, y, z + 1
+    W(1) = (1 - ds(0)) * (1 - ds(1)) * ds(2);
     // x, y + 1, z
     W(2) = (1 - ds(0)) * ds(1) * (1 - ds(2));
-    // x, y, z + 1
-    W(3) = (1 - ds(0)) * (1 - ds(1)) * ds(2);
-    // x + 1, y + 1, z
-    W(4) = ds(0) * ds(1) * (1 - ds(2));
+    // x, y + 1, z + 1
+    W(3) = (1 - ds(0)) * ds(1) * ds(2);
+    // x + 1, y, z
+    W(4) = ds(0) * (1 - ds(1)) * (1 - ds(2));
     // x + 1, y, z + 1
     W(5) = ds(0) * (1 - ds(1)) * ds(2);
-    // x, y + 1, z + 1
-    W(6) = (1 - ds(0)) * ds(1) * ds(2);
+    // x + 1, y + 1, z
+    W(6) = ds(0) * ds(1) * (1 - ds(2));
     // x + 1, y + 1, z + 1
     W(7) = ds(0) * ds(1) * ds(2);
 }
