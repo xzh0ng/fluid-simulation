3c9e7ad3180347677cfb8ccda5e3c932e27c4647
Sitao Wang <sitaowang1998@outlook.com>
2020-12-14 14:43:48 -0500

    Added solid boundary condition

---
+++ include/solid_boundary.h
@@ -0,0 +1,3 @@
+#include <unsupported/Eigen/CXX11/Tensor>
+
+void solid_boundary(Eigen::Tensor<double, 3> &u, Eigen::Tensor<double, 3> &v,Eigen::Tensor<double, 3> &w);
---
+++ src/solid_boundary.cpp
@@ -0,0 +1,34 @@
+#include "solid_boundary.h"
+
+void set_boundary(Eigen::Tensor<double, 3> &t) {
+    unsigned x = t.dimension(0);
+    unsigned y = t.dimension(1);
+    unsigned z = t.dimension(2);
+
+    for (unsigned i = 0; i < x; ++i) {
+        for (unsigned j = 0; j < y; ++j) {
+            t(i, j, 0) = 0;
+            t(i, j, z - 1) = 0;
+        }
+    }
+
+    for (unsigned i = 0; i < x; ++i) {
+        for (unsigned k = 0; k < z; ++k) {
+            t(i, 0, k) = 0;
+            t(i, y - 1, k) = 0;
+        }
+    }
+
+    for (unsigned j = 0; j < y; ++j) {
+        for (unsigned k = 0; k < z; ++k) {
+            t(0, j, k) = 0;
+            t(x - 1, j, k) = 0;
+        }
+    }
+}
+
+void solid_boundary(Eigen::Tensor<double, 3> &u, Eigen::Tensor<double, 3> &v,Eigen::Tensor<double, 3> &w) {
+    set_boundary(u);
+    set_boundary(v);
+    set_boundary(w);
+}
