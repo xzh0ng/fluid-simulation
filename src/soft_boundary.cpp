27c6d5a66cab58bb875d26034597c0c0d65cdd87
Xuhong Zhong <xuhong.zhong@mail.utoronto.ca>
2020-12-15 18:13:41 -0500

    avoid seg fault when i = 0

--- src/soft_boundary.cpp
+++ src/soft_boundary.cpp
@@ -9,19 +9,19 @@ void soft_boundary(Eigen::Tensor<double, 3> &p, Eigen::Tensor<int, 3> cell_state
                 }
 
                 // Check the adjacent fluid cells
-                if (i - 1 >= 0 && cell_state(i - 1, j, k) == 1) {
+                if (i >= 1 && cell_state(i - 1, j, k) == 1) {
                     p(i - 1, j, k) -= 0.5 * p(i, j, k);
                 }
                 if (i + 1 < p.dimension((0)) && cell_state(i + 1, j, k) == 1) {
                     p(i + 1, j, k) -= 0.5 * p(i, j, k);
                 }
-                if (j - 1 >= 0 && cell_state(i, j - 1, k) == 1) {
+                if (j >= 1 && cell_state(i, j - 1, k) == 1) {
                     p(i, j - 1, k) -= 0.5 * p(i, j, k);
                 }
                 if (j + 1 < p.dimension(1) && cell_state(i, j + 1, k) == 1) {
                     p(i, j + 1, k) -= 0.5 * p(i, j, k);
                 }
-                if (k - 1 >= 0 && cell_state(i, j, k - 1) == 1) {
+                if (k >= 1 && cell_state(i, j, k - 1) == 1) {
                     p(i, j, k - 1) -= 0.5 * p(i, j, k);
                 }
                 if (k + 1 < p.dimension(2) && cell_state(i, j, k + 1) == 1) {
