19ecf4f838d08d98973aa9e29a6a35ba383c0e06
Sitao Wang <sitaowang1998@outlook.com>
2020-12-16 16:27:01 -0500

    Fixed solid boundary

--- src/solid_boundary.cpp
+++ src/solid_boundary.cpp
@@ -1,40 +1,55 @@
 #include "solid_boundary.h"
 
-void set_boundary(Eigen::Tensor<double, 3> &t) {
-    unsigned x = t.dimension(0);
-    unsigned y = t.dimension(1);
-    unsigned z = t.dimension(2);
-
-    for (unsigned i = 0; i < x; ++i) {
-        for (unsigned j = 0; j < y; ++j) {
-            t(i, j, 0) = 0;
-            t(i, j, 1) = 0;
-            t(i, j, z - 1) = 0;
-            t(i, j, z - 2) = 0;
-        }
-    }
-
-    for (unsigned i = 0; i < x; ++i) {
-        for (unsigned k = 0; k < z; ++k) {
-            t(i, 0, k) = 0;
-            t(i, 1, k) = 0;
-            t(i, y - 1, k) = 0;
-            t(i, y - 2, k) = 0;
-        }
-    }
-
-    for (unsigned j = 0; j < y; ++j) {
-        for (unsigned k = 0; k < z; ++k) {
-            t(0, j, k) = 0;
-            t(1, j, k) = 0;
-            t(x - 1, j, k) = 0;
-            t(x - 2, j, k) = 0;
-        }
-    }
-}
-
 void solid_boundary(Eigen::Tensor<double, 3> &u, Eigen::Tensor<double, 3> &v,Eigen::Tensor<double, 3> &w) {
-    set_boundary(u);
-    set_boundary(v);
-    set_boundary(w);
+
+    for (unsigned i = 0; i < w.dimension(0); ++i) {
+        for (unsigned j = 0; j < w.dimension(1); ++j) {
+            if (w(i, j, 0) < 0) {
+                w(i, j, 0) = 0;
+            }
+            if (w(i, j , 1) < 0) {
+                w(i, j, 1) = 0;
+            }
+            if (w(i, j, w.dimension(2) - 2) > 0) {
+                w(i, j, w.dimension(2) - 2) = 0;
+            }
+            if (w(i, j, w.dimension(2) - 1) > 0) {
+                w(i, j, w.dimension(2) - 1) = 0;
+            }
+        }
+    }
+
+    for (unsigned i = 0; i < v.dimension(0); ++i) {
+        for (unsigned k = 0; k < v.dimension(2); ++k) {
+            if (v(i, 0, k) < 0) {
+                v(i, 0, k) = 0;
+            }
+            if (v(i, 1, k) < 0) {
+                v(i, 1, k) = 0;
+            }
+            if (v(i, v.dimension(1) - 2, k) > 0) {
+                v(i, v.dimension(1) - 2, k) = 0;
+            }
+            if (v(i, v.dimension(1) - 1, k) > 0) {
+                v(i, v.dimension(1) - 1, k) = 0;
+            }
+        }
+    }
+
+    for (unsigned j = 0; j < u.dimension(1); ++j) {
+        for (unsigned k = 0; k < u.dimension(2); ++k) {
+            if (u(0, j, k) < 0) {
+                u(0, j, k) = 0;
+            }
+            if (u(1, j, k) < 0) {
+                u(1, j, k) = 0;
+            }
+            if (u(u.dimension(0) - 2, j, k) > 0) {
+                u(u.dimension(0) - 2, j, k) = 0;
+            }
+            if (u(u.dimension(0) - 1, j, k) > 0) {
+                u(u.dimension(0) - 1, j, k) = 0;
+            }
+        }
+    }
 }
