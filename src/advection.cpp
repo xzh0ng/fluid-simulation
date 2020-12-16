3026396410192f42deae3733c541daac1ed05e48
Sitao Wang <sitaowang1998@outlook.com>
2020-12-16 18:24:38 -0500

    Bug fix

--- src/advection.cpp
+++ src/advection.cpp
@@ -8,19 +8,19 @@ void advection(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, double h, i
 
     for (unsigned i = 0; i < q.size() / 3; ++i) {
         if (q(3 * i) < 0) {
-            q(3 * 1) = 0;
+            q(3 * i) = 0;
         }
         if (q(3 * i) > h * nx) {
             q(3 * i) = h * nx;
         }
         if (q(3 * i + 1) < 0) {
-            q(3 * 1 + 1) = 0;
+            q(3 * i + 1) = 0;
         }
         if (q(3 * i + 1) > h * ny) {
             q(3 * i + 1) = h * ny;
         }
         if (q(3 * i + 2) < 0) {
-            q(3 * 1 + 2) = 0;
+            q(3 * i + 2) = 0;
         }
         if (q(3 * i + 2) > h * nz) {
             q(3 * i + 2) = h * nz;
