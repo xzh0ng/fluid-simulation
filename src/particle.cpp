1adcfc4e934ad24c7f0eec4e6069c8474734e745
Xuhong Zhong <xuhong.zhong@mail.utoronto.ca>
2020-12-20 01:35:33 -0500

    fix constructor

--- src/particle.cpp
+++ src/particle.cpp
@@ -4,6 +4,7 @@ using namespace Eigen;
 Particle::Particle(double x, double y, double z) {
     prev_pos = Vector3d(x, y, z);
     pos = Vector3d(x, y, z);
+    v = Vector3d(0, 0, 0);
     rho = 0;
     rho_near = 0;
     p = 0;
