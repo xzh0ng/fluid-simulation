16b97e11280acb6b65d1eb59068aa11772370a4a
Xuhong Zhong <xuhong.zhong@mail.utoronto.ca>
2020-12-21 10:41:02 -0500

    fix stuck on boundary

--- src/prep_for_next_step.cpp
+++ src/prep_for_next_step.cpp
@@ -6,17 +6,17 @@ void prep_for_next_step(std::vector<std::shared_ptr<Particle>> &particles) {
         std::shared_ptr<Particle> &p = particles[i];
         p->v = (p->pos - p->prev_pos) / dt;
 
-        if (p->pos(0) <= -canvas_x) {
-            p->pos(0) = -canvas_x;
+        if (p->pos(0) < -canvas_x) {
+            p->pos(0) = -2 * canvas_x - p->pos(0);
             if (p->v(0) < 0) p->v(0) *= -1 * damp;
         }
         if (p->pos(0) > canvas_x) {
-            p->pos(0) = canvas_x;
+            p->pos(0) = 2 * canvas_x - p->pos(0);
             if (p->v(0) > 0) p->v(0) *= -1 * damp;
         }
 
-        if (p->pos(1) <= 0) {
-            p->pos(1) = 0;
+        if (p->pos(1) < 0) {
+            p->pos(1) = -p->pos(1);
             if (p->v(1) < 0) p->v(1) *= -1 * damp;
         }
         if (p->pos(1) > canvas_y) {
@@ -24,12 +24,12 @@ void prep_for_next_step(std::vector<std::shared_ptr<Particle>> &particles) {
             if (p->v(1) > 0) p->v(1) *= -1 * damp;
         }
 
-        if (p->pos(2) <= -canvas_z) {
-            p->pos(2) = -canvas_z;
+        if (p->pos(2) < -canvas_z) {
+            p->pos(2) = -2 * canvas_z - p->pos(2);
             if (p->v(2) < 0) p->v(2) *= -1 * damp;
         }
         if (p->pos(2) > canvas_z) {
-            p->pos(2) = canvas_z;
+            p->pos(2) = 2 * canvas_z - p->pos(2);
             if (p->v(2) > 0) p->v(2) *= -1 * damp;
         }
         
